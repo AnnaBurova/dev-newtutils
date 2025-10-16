b947129e73f880400d73ec10b2e6d77960a3599c
Anna Burova <50639099+AnnaBurova@users.noreply.github.com>
2025-10-17 01:37:00 +0300

    feat(error_msg): Add NewtUtils module with error_msg utility
    Initial setup for the NewtUtils Python module, including project structure, documentation, and licensing updates. Implements error_msg function in console.py using colorama for colored output, adds basic tests, and configures pyproject.toml for packaging and dependencies.

---
+++ newtutils/console.py
@@ -0,0 +1,35 @@
+"""
+Created on 2025-10
+
+@author: NewtCode Anna Burova
+"""
+
+from colorama import Fore, Style
+
+def error_msg(*args: object, stop: bool = True) -> None:
+    """
+    Print error messages in red to the console and optionally stop the program.
+
+    This function prints one or more messages in bright red color using colorama.
+    It can be used for CLI tools, scripts, or any console-based feedback.
+
+    Parameters:
+        *args (object): One or more messages or objects to print.
+        stop (bool): by default True.
+
+    Returns:
+        None
+
+    Raises:
+        SystemExit: If stop=True, terminates the program with exit code 1.
+    """
+
+    message = "\n".join(str(arg) for arg in args)
+
+    print(Style.BRIGHT, Fore.RED)
+    print("::: ERROR :::")
+    print(message)
+    print(Style.RESET_ALL)
+
+    if stop:
+        raise SystemExit(1)
